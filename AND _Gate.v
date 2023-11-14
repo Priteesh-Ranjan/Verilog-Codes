f10c5aa659ef7e8882239ebccc5cb0c3a8911218
Priteesh-Ranjan <122392732+Priteesh-Ranjan@users.noreply.github.com>
2023-11-15 00:34:52 +0530

    Create AND _Gate.v

---
+++ AND _Gate.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.01.2023 00:20:39
+// Design Name: 
+// Module Name: or_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module or_gate(a,b,c);
+input a,b;
+output c;
+assign c = a|b;
+endmodule
