6b40b67203277096ccb8e273e3cb58ee4ce8fc89
Priteesh-Ranjan <122392732+Priteesh-Ranjan@users.noreply.github.com>
2023-11-15 10:36:09 +0530

    Update 8x1mux_TestBench.v

--- 8x1mux_TestBench.v
+++ 8x1mux_TestBench.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.02.2023 20:29:44
-// Design Name: 
-// Module Name: tb_8bitfmult
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module tb_8bitfmult();
 reg [7:0]a,b;
